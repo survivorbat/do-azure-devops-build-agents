fdeb6d13b8737c8401f092c5eabe6e08fd185200
Maarten van der Heijden <maarten.van.der.heijden@ing.com>
2020-07-03 22:14:17 +0200

    master: Add initial setp

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "digitalocean" {
+  token = var.apiToken
+}
+
+resource "digitalocean_droplet" "droplet" {
+  count = var.droplet_amount
+  image = "ubuntu-18-04-x64"
+  name = "build_agent_${count.index}"
+  region = var.droplet_region
+  size = var.droplet_size
+
+  provisioner "local-exec" {
+    command = "ansible-playbook ../ansible/site.yaml -u root -i ${join(",", digitalocean_droplet.droplet.*.ipv4_address)}"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "droplet_amount" {
+  type = number
+  default = "__dropletAmount__"
+}
+
+variable "droplet_size" {
+  type = string
+  default = "__dropletSize__"
+}
+
+variable "apiToken" {
+  type = string
+  default = "__apiToken__"
+}
+
+variable "droplet_region" {
+  type = string
+  default = "__dropletRegion__"
+}
