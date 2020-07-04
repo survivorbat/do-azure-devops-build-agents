d763012423ce2d8077a172a04b495eef1300c6f4
Maarten van der Heijden <maarten.van.der.heijden@ing.com>
2020-07-04 14:58:22 +0200

    master: Add state variables

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,12 @@
 terraform {
-
+  backend "s3" {
+    bucket = "__terraformBackendBucket__"
+    key = "__terraformBackendKey__"
+    access_key = "__doAccessKey__"
+    secret_key = "__doSecretKey__"
+    endpoint = "https://ams3.digitaloceanspaces.com"
+    region = "eu-west-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+  }
 }
